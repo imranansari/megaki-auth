ba9087479b5ab5d028c55a69ddeec90f2680604b
imranansari <imran.iansari@gmail.com>
2014-02-22 17:41:49 -0800

    grape config & starter

--- app/api/megaki.rb
+++ app/api/megaki.rb
@@ -2,18 +2,7 @@ module Megaki
   class API < Grape::API
 
 
-    version 'v1', using: :header, vendor: 'megaki'
-    format :json
 
-    resource :auth do
-
-      desc "return moment."
-      get :moment do
-
-        {"name" => "megaki"}
-      end
-
-    end
 
   end
 
