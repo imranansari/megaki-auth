f9d712cb23b1694ef654682a3d83c06fd7d44046
kumar <kumar.adventure@gmail.com>
2014-03-15 00:45:47 +0530

    added field

--- app/controllers/authentications_controller.rb
+++ app/controllers/authentications_controller.rb
@@ -1,11 +1,12 @@
 class AuthenticationsController < ApplicationController
   
   def create
-  	user = User.where(email: params[:user][:email]).first
+    auth = params['auth']
+  	user = User.where(email: auth[:email]).first
   	account_type = AccountType.where(name: auth['type'])
   	if user.blank?
-  		user = User.create(email: params[:user][:email])
-  		user.user_accounts.create(access_token: params[:user][:access_token], account_type_id: account_type.id)
+  		user = User.create(email: auth[:email], first_name: auth[:first_name], last_name: auth[:last_name], company_name: auth[:company_name])
+  		user.user_accounts.create(access_token: auth[:access_token], account_type_id: account_type.id)
   	else
   		user_account = user.user_accounts.where(account_type_id: account_type.id).first
   		user_account.update_attributes(access_token: params[:user][:access_token])
